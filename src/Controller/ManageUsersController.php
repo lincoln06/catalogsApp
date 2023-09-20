6322fa316429d27723a368d3111de6d59a3f7570
Maciej Janta-Lipiński <maciekjl1993@gmail.com>
2023-09-20 20:57:33 +0200

    Added user deleting option

--- src/Controller/ManageUsersController.php
+++ src/Controller/ManageUsersController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Services\GetUsersListService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +15,23 @@ class ManageUsersController extends AbstractController
     #[Route('/manage/users', name: 'app_manage_users')]
     public function index(GetUsersListService $getUsersListService): Response
     {
-        $usersList = $getUsersListService->getUsersFromDatabase();
         return $this->render('manage_users/index.html.twig', [
-            'usersList' => $usersList,
+            'usersList' => $getUsersListService->getUsersFromDatabase(),
+        ]);
+    }
+    #[Route('/manage/users/delete/{id}', name: 'app_delete_user')]
+    public function delete(Request $request, EntityManagerInterface $entityManager, int $id, GetUsersListService $getUsersListService): Response
+    {
+        $user = $entityManager->getRepository(User::class)->find($id);
+        $message = '';
+        if (!$user) {
+            $message = 'Brak danych';
+        }
+        $entityManager->remove($user);
+        $entityManager->flush();
+        return $this->render('manage_users/index.html.twig', [
+            'message' => $message,
+            'usersList' => $getUsersListService->getUsersFromDatabase()
         ]);
     }
 }
